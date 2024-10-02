d665845c91d276cc5a0f981e2519cf3a3bc3d7cf
Michal Presz <m.presz@wp.pl>
2024-10-02 05:50:55 +0100

    new colour

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 console.log("Conditionals fun!");
 
 if (5 > 3) {
-console.log("hello");
+  console.log("hello");
+}
 
+let themeAnswer = prompt("Do you prefer a white or black?");
+
+if (themeAnswer === "white") {
+  document.body.style.backgroundColor = themeAnswer;
+} else if (themeAnswer === "black") {
+  document.body.style.backgroundColor = themeAnswer;
+} else {
+  document.body.style.backgroundColor = themeAnswer;
 }
